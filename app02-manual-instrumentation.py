58f199fccc0c516224c0e4c7c28f281f290a5107
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2025-02-10 21:37:31 +0100

    add program to test manual instrumentation

--- app02-manual-instrumentation.py
+++ app02-manual-instrumentation.py
@@ -1,14 +1,23 @@
 from random import randint
 from flask import Flask
-from opentelemetry import trace
+from opentelemetry import trace, metrics
 import logging
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Acquire a tracer
-tracer = trace.get_tracer("counter.tracer")
+# Acquire a tracer.
+tracer = trace.get_tracer('counter.tracer')
+
+# Acquire a meter.
+meter = metrics.get_meter('counter.meter')
+
+# Create a counter.
+increase_counter = meter.create_counter(
+    'increase.counts',
+    description='The number of increases by increase value',
+)
 
 
 class Counter:
@@ -20,11 +29,12 @@ class Counter:
         return str(self._count)
 
     def increment(self):
-        with tracer.start_as_current_span('counter') as span:
-            value = randint(1, 5)
-            self._count += value
-            span.set_attribute('counter.increase', value)
-            span.set_attribute('counter.value', self._count)
+        with tracer.start_as_current_span('counter') as counter_span:
+            increase = randint(1, 5)
+            self._count += increase
+            counter_span.set_attribute('counter.increase', increase)
+            counter_span.set_attribute('counter.value', self._count)
+            increase_counter.add(1, {'counter.increase': increase})
 
 
 cnt = Counter()
